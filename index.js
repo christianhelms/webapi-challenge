8ba35b010a7c9ee633bd687c22aeb666ccb47bd0
christianhelms <helmsmedia@gmail.com>
2019-08-10 18:09:34 -0500

    :art: added outputs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require('./server');
+const server = require("./server");
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => console.log("API on port 8000"));
--- server.js
+++ server.js
@@ -38,43 +38,50 @@ let people = [
 let chores = [
   {
     id: 1,
-    chore: "take out the trash",
+    description: "take out the trash",
+    notes: "to the curb",
     assignedTo: 1,
     completed: false
   },
   {
     id: 2,
-    chore: "wash the car",
+    description: "wash the car",
+    notes: "soon",
     assignedTo: 2,
     completed: false
   },
   {
     id: 3,
-    chore: "dailies on wow",
+    description: "dailies on wow",
+    notes: "in Nazjataar",
     assignedTo: 3,
     completed: false
   },
   {
     id: 4,
-    chore: "clean the bathroom",
+    description: "clean the bathroom",
+    notes: "spotless",
     assignedTo: 4,
     completed: false
   },
   {
     id: 5,
-    chore: "dust",
+    description: "dust",
+    notes: "",
     assignedTo: 5,
     completed: false
   },
   {
     id: 6,
-    chore: "vaccum",
+    description: "vaccum",
+    notes: "",
     assignedTo: 6,
     completed: false
   },
   {
     id: 7,
-    chore: "mop",
+    description: "mop",
+    notes: "",
     assignedTo: 7,
     completed: false
   }
@@ -86,6 +93,62 @@ server.get("/", (req, res) => {
   res.status(200).json({ message: "API functional" });
 });
 
+// return all people
 
+server.get("/people", (req, res) => {
+  res.status(200).json(people);
+});
+
+// return all chores
+
+server.get("/chores", (req, res) => {
+  const completed = req.query.sortby || "completed";
+  const sortedRes = chores.sort((t, f) =>
+    t[completed] < f[completed] ? -1 : 1
+  );
+  res.status(200).json(sortedRes);
+});
+
+// chores by id
+
+server.get("/people/:id/chores", (req, res) => {
+  const id = Number(req.params.id);
+  if (id) {
+    const chore = chores.filter(chore => chore.assignedTo === id);
+    res.status(200).json(chore);
+  } else {
+    res.status(404).json({ message: "Could not find ID" });
+  }
+});
+
+// create a chore
+server.post("/chores", (req, res) => {
+  addChore = req.body;
+  if (
+    req.body.description &&
+    req.body.id &&
+    req.body.completed &&
+    req.body.assignedTo
+  ) {
+    chores.push(addChore);
+    res.status(200).json(addChore);
+  } else {
+    res.status(400).json({
+      message: "All fields must be complete"
+    });
+  }
+});
+
+// delete a chore
+
+server.delete("/chores/:id", (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    chores.splice(id, 1);
+    res.status(201).json({ message: "chore gone" });
+  } else {
+    res.status(404).json({ message: "chore not found" });
+  }
+});
 
 module.exports = server;
